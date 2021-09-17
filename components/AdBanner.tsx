3599e098c606d36b66599723d35b12dc4b33ca91
souravrs999 <souravraveendran6@gmail.com>
2021-09-17 08:59:57 +0530

    [sq] fix #1

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -1,15 +1,11 @@
 import AdSense from "react-ssr-adsense";
 
-interface Window {
-  adsbygoogle: any;
-}
-
 export default function AdBanner() {
   return (
     <AdSense
       className="adsbygoogle adbanner-customize"
-      data-ad-client="ca-pub-5775610330008624"
-      data-ad-slot="7806394673"
+      client="ca-pub-5775610330008624"
+      slot="7806394673"
     />
   );
 }
